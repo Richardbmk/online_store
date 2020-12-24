7f7e7779a77e5b271eda7771783849a5d734e93f
Ricardo <rdobmk@gmail.com>
2020-12-24 12:35:10 +0100

    basic authentication implemented

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const bodyParser = require('body-parser');
 const mongoose = require('mongoose');
 const session = require('express-session');
 const MongoDBStore = require('connect-mongodb-session')(session);
+const csrf = require('csurf');
 
 const User = require('./models/user');
 
@@ -15,11 +16,13 @@ const store = new MongoDBStore({
     uri: db_url_cloud,
     collection: 'sessions'
 });
+const csrfProtection = csrf();
 
 const shopRoutes = require('./routes/shop');
 const adminRoutes = require('./routes/admin');
 const authRoutes = require('./routes/auth');
 const errorController = require('./controllers/error');
+const flash = require('connect-flash');
 
 app.set('view engine', 'ejs');
 app.use(bodyParser.urlencoded({extended: true}));
@@ -31,6 +34,9 @@ app.use(session( {
     store: store
 }));
 
+app.use(csrfProtection);
+app.use(flash());
+
 app.use((req, res, next) => {
     if (!req.session.user) {
         return next();
@@ -41,6 +47,12 @@ app.use((req, res, next) => {
         next();
     })
     .catch(err => console.log(err));
+});
+
+app.use((req, res, next) => {
+    res.locals.isAuthenticated = req.session.isLoggedIn;
+    res.locals.csrfToken = req.csrfToken();
+    next();
 })
 app.use('/admin', adminRoutes);
 app.use(shopRoutes);
@@ -50,19 +62,7 @@ app.use(errorController.get404);
 
 mongoose
     .connect(db_url_cloud)
-    .then(result => {
-        User.findOne().then(user => {
-            if (!user) {
-                const user = new User({
-                    name: "Richard",
-                    email: 'richard@gmail.com',
-                    cart: {
-                        items: []
-                    }
-                });
-                user.save();
-            }
-        });
+    .then(() => {
         app.listen(3000, () =>{
             console.log("This server in running in port 3000");
         }); 
--- controllers/shop.js
+++ controllers/shop.js
@@ -8,8 +8,7 @@ exports.getIndex = (req, res, next) => {
             res.render('shop/index', {
                 prods: products,
                 pageTitle: 'Shop',
-                path: '/',
-                isAuthenticated: req.session.isLoggedIn
+                path: '/'
             });
         })
         .catch(err => {
@@ -25,7 +24,6 @@ exports.getProducts = (req, res, next) => {
                 prods: products,
                 pageTitle: 'All Products',
                 path: '/products',
-                isAuthenticated: req.session.isLoggedIn
             });
         })
         .catch(err => {
@@ -40,8 +38,7 @@ exports.getProduct = (req, res, next) => {
             res.render('shop/product-detail', {
                 product: product,
                 pageTitle: product.title,
-                path: '/products',
-                isAuthenticated: req.session.isLoggedIn
+                path: '/products'
             });
         })
         .catch(err => console.log(err));
@@ -57,8 +54,7 @@ exports.getCart = (req, res, next) => {
             res.render('shop/cart', {
                 path: '/cart',
                 pageTitle: 'Your Cart',
-                products: products,
-                isAuthenticated: req.session.isLoggedIn
+                products: products
             });
         })
         .catch(err => console.log(err));
@@ -96,7 +92,7 @@ exports.postOrder = (req, res, next) => {
             });
             const order = new Order({
                 user: {
-                    name: req.user.name,
+                    email: req.user.email,
                     userId: req.user
                 },
                 products: products
@@ -118,8 +114,7 @@ exports.getOrders = (req, res, next) => {
             res.render('shop/orders', {
                 path: '/orders',
                 pageTitle: 'Your Orders',
-                orders: orders,
-                isAuthenticated: req.session.isLoggedIn
+                orders: orders
             });
         })
         .catch(err => console.log(err));
