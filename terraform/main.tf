858cb6bd5f229a388e9e88863dae4381deab88d1
Ricardo <rdobmk@gmail.com>
2021-01-11 13:59:57 +0100

    terraform backend configured

--- terraform/main.tf
+++ terraform/main.tf
@@ -1,7 +1,14 @@
-resource "aws_s3_bucket" "finance" {
-    bucket = "finance-20201226"
-    tags = {
-        Description = "Finance and Payroll"
-    }
+provider "aws" {
+  region  = "us-east-2"
+  version = "~> 2.50.0"
 }
 
+terraform {
+  backend "s3" {
+    bucket         = "rick-ecommerce-devops-tfstate"
+    key            = "ecommerce-app.tfstate"
+    encrypt        = true
+    region         = "us-east-2"
+    dynamodb_table = "rick-ecommerce-devops-tfstate"
+  }
+} 
